df26077d6e7454808c121016e582390514f215d6
Shaheem <mshaheemkz@gmail.com>
2024-02-14 15:06:10 +0500

    removed unused vars

--- src/components/MessageItem.js
+++ src/components/MessageItem.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import { Image } from 'react-bootstrap';
 import { FiPhoneMissed } from "react-icons/fi";
 import AudioPlayer from './AudioPlayer';
